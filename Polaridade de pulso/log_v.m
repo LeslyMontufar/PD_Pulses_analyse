f687077b35e5f38bc4807e8bf07e4e5e32079414
Lesly Montúfar <leslymontufarj@gmail.com>
2022-08-11 17:25:53 -0300

    Verificando polaridade

--- Polaridade de pulso/log_v.m
+++ Polaridade de pulso/log_v.m
@@ -1,5 +1,5 @@
 function [v,naocoincidem,i_erros] = log_v(myiPeak,iPeak,ss)
-    v_erros = abs((myiPeak(:)-iPeak(:))./iPeak(:)*100);
+    v_erros = (myiPeak(:)~=(iPeak(:)./abs(iPeak(:)))*100;
     v = [myiPeak(:),iPeak(:), v_erros];
     n = 1:size(iPeak,1);
     i_erros = n(v_erros>10);
--- Polaridade de pulso/main.m
+++ Polaridade de pulso/main.m
@@ -1,81 +1,12 @@
-clc;
-close all; 
-clearvars;
+clc;close all; clearvars;
 load('dadosLesly.mat');
 
 myiPeak = ones(length(iPeak),1);
-if ~verifica_picos(myiPeak,iPeak,pulsos); return; end
 
 % ---- Determinando se a primeira oscilação é um mínimo ou máximo local ----
-err_eq = 0.0045;
-% i = 51;
-% r = 30;
-% kk = .8;
-% pp = 15;
-% pulso = pulsos(i,:);
-% ipulso2 = 1:1/r:size(pulso,2)+1-1/r;
-% pulso2 = spline(1:size(pulso,2),pulso,ipulso2);
- 
 
-% figure;
-% plot(pulso);
-% hold on;
-% plot(ipulso2,pulso2);
-% line ([size(pulso,2) 0], [iPeak(i,1) iPeak(i,1)], "linestyle", "-", "color", "g");
-% 
-% ipulso3 = 1:1/r:size(pulso,2)+1-1/r;
-% pulso3 = resample(pulso,r,1);
-% plot(ipulso3,pulso3);
-
-% metodo
-% sub = 0;
-% myiPeak_ = abs(myiPeak(i));
-% n = 1:size(pulso,2);
-% k = floor(kk/100*size(pulso,2));
-% pulso_mm = conv(pulso,ones(1,k)/k,'same');
-% derivada = diff(pulso_mm);
-% derivada = [0 derivada];
-% 
-% ider = 1:1/r:size(derivada,2)+1-1/r;
-% der = spline(1:size(derivada,2),derivada,ider);
-% der0 = round(ider(abs(der)<=1/100*max(abs(der))));
-% [~,der0] = findgroups(der0);
-% 
-% s = pulso_mm(der0);
-% n = n(der0);
-% 
-% condicao = (abs(pulso_mm(der0))>=.4*err_eq);%pp/100*max(abs(pulso_mm(der0)))); 
-% s = s(condicao);
-% n = n(condicao);
-% myiPeak_ = s(1)/abs(s(1)) * myiPeak_;
-% 
-% if sub == 0
-%     fh = figure;
-%     fh.WindowState = 'maximized';
-% end
-% plot(pulso);
-% hold on;
-% plot(pulso_mm);
-% line ([size(pulso,2) 0], [iPeak(i,1) iPeak(i,1)], "linestyle", "-", "color", "g"); 
-% scatter(n,s,'r','filled');
-% 
-% plot(.05*max(abs(pulso))/max(abs(derivada))*derivada);
-% 
-% v_err = abs((myiPeak_-iPeak(i,1))/iPeak(i,1)*100);
-% title([i,myiPeak_,iPeak(i,1), v_err]);
-% xlim([0 size(pulso,2)]);
-% if sub == 0
-%     legend("original","mm","derivada");
-% end
-
-% return;
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-% plot_pulso_derivada_erro(2,pulsos,myiPeak,iPeak);
-
-% Dado: 0.0045 é o passo em y, é erro mínimo do equipamento
-% Tem mais erros qnd iPeak ~ 0.25, logo qnd o erro é 1.8% do pico de
-% corrente.
+err_eq = 0.0045; % 20%
+min_max_pulsos = 0.02256; % 5*err_eq
 
 % Gerando resultado final
 r = 30;
@@ -83,23 +14,31 @@ kk = .8;
 pp = 15;
 k = floor(kk/100*size(pulsos,2)*r);
 derivada = zeros(1,size(pulsos,2)*r);
+n = 1:size(pulsos,2)*r;
 for i=1:size(pulsos,1)
     pulso = pulsos(i,:);
     pulso = resample(pulso,r,1);
-    n = 1:size(pulso,2);
     
     % Média móvel
     pulso_mm = conv(pulso,ones(1,k)/k,'same');
     derivada(2:end) = diff(pulso_mm);
 
-    condicao = ((abs(derivada)<=1/100*max(abs(derivada))) & (abs(pulso_mm)>=pp/100*myiPeak(i))); 
+    condicao = ((abs(derivada)<=1/100*max(abs(derivada))) & (abs(pulso_mm)>=pp/100*max(abs(pulso_mm)))); 
     s = pulso_mm(condicao);
     myiPeak(i) = s(1)/abs(s(1)) * myiPeak(i);
     if iPeak(i)/abs(iPeak(i)) ~= myiPeak(i)/abs(myiPeak(i))
         ss(end+1) = s(1);
     end
 end
-[v,naocoincidem,i_erros] = log_v(myiPeak,iPeak,ss);
+iPolaridade = iPeak./abs(iPeak);
+v_erros = (myiPeak(:)~=iPolaridade)*100;
+v = [myiPeak(:),iPolaridade(:), v_erros];
+n = 1:size(iPolaridade,1);
+condicao = (v_erros==100);
+i_erros = n(condicao);
+naocoincidem = [i_erros',myiPeak(condicao),iPolaridade(condicao),v_erros(condicao),ss'];
+
+% [v,naocoincidem,i_erros] = log_v(myiPeak,iPeak,ss);
 
 % Para testar
 % close all;
@@ -109,9 +48,9 @@ for sub=1:9
     i=i_erros(sub);
     subplot(3,3,sub);
     plot_pulso_derivada_erro(i,pulsos,myiPeak,iPeak,r,kk,pp,1,0);
-%     plot_pulso_erro(i,pulsos,iPeak,"~");
 end 
 
-% plot_pulso_erro(550,pulsos,iPeak,0);
-% plot_pulso_erro(286,pulsos,iPeak,0);
-% plot_pulso_derivada_erro(552,pulsos,myiPeak,iPeak,kk,pp,0);
+figure;
+plot(iPeak(i_erros));
+
+fprintf("\nNão coincidem: %d\n\n",numel(i_erros));
